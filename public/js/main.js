39204f661d59019bd3e341f4c0a1b5499e1dc33f
aniket1004 <aniketdhole1004@gmail.com>
2021-03-16 12:24:30 +0530

    Project files

---
+++ public/js/main.js
@@ -0,0 +1,79 @@
+
+
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name');
+const userList = document.getElementById('users');
+
+// Get username and room from URL
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true
+});
+
+const socket = io();
+
+// Join chatroom
+socket.emit('joinRoom', { username, room });
+
+// Get room and users
+socket.on('roomUsers', ({ room, users }) => {
+  outputRoomName(room);
+  outputUsers(users);
+});
+
+// Message from server
+socket.on('message', message => {
+  console.log(message);
+  outputMessage(message);
+
+  // Scroll down
+  chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+// Message submit
+chatForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // Get message text
+  const msg = e.target.elements.msg.value;
+
+  // Emit message to server
+  socket.emit('chatMessage', msg);
+
+  // Clear input
+  e.target.elements.msg.value = '';
+  e.target.elements.msg.focus();
+});
+
+// Output message to DOM
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  if (message.username == username)
+  {
+    div.innerHTML = `<p class="meta" style="text-align:right;margin-right:23px;">You <span>${message.time}</span></p>
+    <p class="text" style="text-align:right;margin-right:23px;">
+      ${message.text}
+    </p>`;
+  }
+  else
+  {
+    div.innerHTML = `<p class="meta" style="text-align:left;">${message.username} <span>${message.time}</span></p>
+    <p class="text" style="text-align:left">
+      ${message.text}
+    </p>`;
+  }
+  document.querySelector('.chat-messages').appendChild(div);
+}
+
+// Add room name to DOM
+function outputRoomName(room) {
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users) {
+  userList.innerHTML = `
+    ${users.map(user => `<li><i class="fa fa-check-circle"></i> <u> ${user.username}</u></li>`).join('')}
+  `;
+}
